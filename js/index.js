d8fca7ef078df53674832561b3ad10facad34fe0
JenyaCherny <jenya.cherny@gmail.com>
2024-12-27 21:50:34 -0500

    did experiments with if and if,if, else and commented out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*let healthPoints = 11;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+let gamePoints = 2;
+
+if (gamePoints >= 5) {
+    console.log("You are doing better");
+}
+else {
+    console.log("You need to catch up");
+}
+
+let VictorGrade = 2;
+if (VictorGrade >=4) {
+    console.log("You did good this semestr");
+}
+if (VictorGrade == 5) {
+    console.log("Great job!");
+} else {
+    console.log("You need to study more");
+}
+    */
